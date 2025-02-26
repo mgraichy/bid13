a99c798231e8cbbc57f6652e9be74b70e885984b
Marlon Graichy <mgraichy@gmail.com>
2025-02-26 14:21:32 -0500

    separate curl into request and response
    * fix the LLM's searching for non-existent array members
    
    * return bool or null from isValidPhoneNumber(), to account for numbers
        which don't fit into the response options from TeleSign

--- src/telesign.php
+++ src/telesign.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-function isValidPhoneNumber($apiUrl, $phoneNumber, $auth)
+function setUpCurl(string $auth, string $apiUrl, string $phoneNumber): array
 {
     $headers = [
         "Authorization: Basic $auth",
@@ -9,7 +9,6 @@ function isValidPhoneNumber($apiUrl, $phoneNumber, $auth)
         'Content-Type: application/json; charset=utf-8',
     ];
 
-    // https://developer.telesign.com/enterprise/reference/submitphonenumberforidentityalt#form-submitPhoneNumberForIdentityAlt:
     $requestData = json_encode(['phone_number' => $phoneNumber,]);
 
     // Add all curl options in a single array for readability.
@@ -22,29 +21,57 @@ function isValidPhoneNumber($apiUrl, $phoneNumber, $auth)
         CURLOPT_HTTPHEADER => $headers,
     ];
 
+    return $options;
+}
+
+
+/**
+ * Checks the validity of the number with TeleSign's Phone ID.
+ *
+ * @param array $curlRequest
+ * @return boolean|null The null is if the given number did not belong to any of the options
+ */
+function isValidPhoneNumber(array $curlRequest): ?bool
+{
     $ch = curl_init();
-    curl_setopt_array($ch, $options);
+    curl_setopt_array($ch, $curlRequest);
 
     $response = curl_exec($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    // If there's an error with cURL itself (and not the TeleSign domain):
+    $curlError = curl_errno($ch);
+    if ($curlError) {
+        return false;
+    }
+
     curl_close($ch);
 
-    if ($http_code !== 200) {
-        return false; // API request failed
+    // https://developer.telesign.com/enterprise/reference/submitphonenumberforidentityalt
+    // says 300 is successful, 301 if only able to get partial data of the phone ID:
+    if ($httpCode >= 200 && $httpCode < 302) {
+        // Shouldn't json_decode unless it's successful:
+        $responseArray = json_decode($response, true);
+        // The $data array was checking for non-existent members etc.:
+        $extraction = $responseArray['phone_type']['description'] ?? null;
+        // str_replace is faster than preg_replace():
+        $type = $extraction ? strtolower(trim(str_replace('_', '', $extraction))) : null;
+
+        // PHP8+:
+        return match ($type) {
+            'prepaid', 'voip', 'invalid', 'payphone', 'restricted' => false,
+            'valid', 'fixedline', 'mobile' => true,
+            default => null
+        };
     }
 
-    $data = json_decode($response, true);
-    if (!isset($data['numbering']['phone_type'])) {
-        return false; // Unexpected API response
-    }
-
-    $valid_types = ["FIXED_LINE", "MOBILE", "VALID"];
-    return in_array(strtoupper($data['numbering']['phone_type']), $valid_types);
+    return null;
 }
+
 
 // As this is a POST request, I'd rather include a body (and not put the number in the path):
 $apiUrl = 'https://rest-ww.telesign.com/v1/phoneid';
-$phoneNumber = '1234567890';
+$phoneNumber = '15145534258';
 // $customerId = '<ID>';
 $customerId = '5172B3B5-23B4-4D23-8475-72E342561A23';
 // $apiKey = '<API_KEY>';
@@ -52,7 +79,11 @@ $apiKey = 'oAfhXKHJJw9d9YcOCYhl3pNJenj1G8Juii7m5dlmji0s0UV2NE5mUWOWjVAMIfKJ8yrlC
 // https://developer.telesign.com/enterprise/docs/authentication#basic-authentication:
 $preAuth = base64_encode(mb_convert_encoding("$customerId:$apiKey", 'UTF-8', mb_list_encodings()));
 $auth = mb_convert_encoding($preAuth, 'UTF-8', mb_list_encodings());
-$result = isValidPhoneNumber($apiUrl, $phoneNumber, $auth);
-var_dump($result);
+$curlRequest = setUpCurl($auth, $apiUrl, $phoneNumber);
+var_dump($curlRequest);
+$curlResponse = isValidPhoneNumber($curlRequest);
+
+// $result = isValidPhoneNumber($apiUrl, $phoneNumber, $auth);
+// var_dump($result);
 
 
